9d3c3bb412ac497edd5a048a35058c5856f6e347
Jose-Nortes <jsnd@gft.com>
2023-05-17 11:25:13 +0200

    first get method implemented

--- src/main/java/guru/springframework/spring6restmvc/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/BeerService.java
@@ -2,8 +2,11 @@ package guru.springframework.spring6restmvc;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBears();
+
     Beer getBeerById(UUID id);
 }
