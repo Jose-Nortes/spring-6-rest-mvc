7cc42213e14b4a686f092b0171079082b69d473f
Jose-Nortes <jsnd@gft.com>
2023-05-18 08:37:21 +0200

    get customer exercise

--- src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
@@ -4,25 +4,28 @@ import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
-@RestController("/api/v1/customer")
+@RestController()
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public List<Customer> listCustomer() {
         return customerService.listCustomers();
     }
 
     @RequestMapping("{customerId}")
-    public Customer getCustomerById(@PathParam("customerId") UUID customerId) {
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId) {
         return customerService.getCustomerById(customerId);
     }
 }
