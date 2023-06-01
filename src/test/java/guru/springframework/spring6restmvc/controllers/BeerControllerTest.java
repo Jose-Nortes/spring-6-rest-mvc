b15bec30bf24ba34dee1f0ec29dee0fc6cdb0ff6
Jose-Nortes <jsnd@gft.com>
2023-06-01 08:40:23 +0200

    creating json responses with jayway jsonpath dependency

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -10,13 +10,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/*import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;*/
 
 
 @WebMvcTest(BeerController.class)
@@ -36,12 +38,18 @@ class BeerControllerTest {
         Beer testBeer = beerServiceImpl.listBears().get(0);
 
         // Tells mockito to return this value --so the value is mocked--
-        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        // given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+        // mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // $ means from the root of the document
+                // jsonPath from Jayway JsonPath dependency --> see https://github.com/json-path/JsonPath
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect((jsonPath("$.beerName", is(testBeer.getBeerName()))));
 
 
     }
